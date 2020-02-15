63f840b567de6459fee7f5bffa300d2f7551045c
Joshua Maliwat <mijanowa@gmail.com>
2020-02-15 00:54:15 -0800

    Updates

---
+++ precache-manifest.93e1891f6e978d0fa4329c664744fd30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d14bae2c11949ba7fdf5607e3010b2e",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "d9829df4b68ea7013f40",
+    "url": "/React/static/css/main.8199b7bd.chunk.css"
+  },
+  {
+    "revision": "9c517c22a549790e1c28",
+    "url": "/React/static/js/2.86797bee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React/static/js/2.86797bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9829df4b68ea7013f40",
+    "url": "/React/static/js/main.030c75b6.chunk.js"
+  },
+  {
+    "revision": "5869549981d0c2c3473c",
+    "url": "/React/static/js/runtime-main.8632fba4.js"
+  }
+]);
